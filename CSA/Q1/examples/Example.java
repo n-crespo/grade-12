7f537ec6fee52ec915ab020a63768da462052611
nicolas <nicolasecrespo@gmail.com>
2023-11-03 23:58:25 -0400

    some stuff idek

--- CSA/Q1/examples/Example.java
+++ CSA/Q1/examples/Example.java
@@ -2,6 +2,10 @@ public class Example {
   public static void main(String[] args) {
 
     Math.abs(-5);
+    int x = 5;
+    int z = 10;
+    System.out.println(Math.abs(x) > Math.abs(z));
+    Math.abs(x > z);
     System.out.println(Math.abs(-5));
   }
 }
