0b2ce07c320624e6a7e4a942c5b7e4a972e721b3
Nicolas <nicolasecrespo@gmail.com>
2023-11-21 00:35:41 -0500

    new assignments

--- csa/1/reverse/Reverse.java
+++ csa/1/reverse/Reverse.java
@@ -2,13 +2,15 @@ import java.util.Scanner;
 
 public class Reverse {
   public static void main(String[] args) {
-    System.out.print("Enter your number: ");
-    Scanner scan = new Scanner(System.in);
-    String input = scan.nextLine();
-    StringBuilder input1 = new StringBuilder();
-    input1.append(input);
-    input1.reverse();
-    System.out.println(input1);
-    scan.close();
+    // System.out.print("Enter your number: ");
+    // Scanner scan = new Scanner(System.in);
+    // String input = scan.nextLine();
+    // StringBuilder input1 = new StringBuilder();
+    // input1.append(input);
+    // input1.reverse();
+    // System.out.println(input1);
+    System.out.println(121 % 1000 / 100);
+    System.out.println(121 % 10);
+    // scan.close();
   }
 }
---
+++ foo.java
@@ -0,0 +1,12 @@
+public class foo {
+  public static void main(String[] args) {
+    int x = 0;
+    while (true) {
+      System.out.println("Hello!");
+      x++;
+      if (x == 10) {
+        break;
+      }
+    }
+  }
+}
