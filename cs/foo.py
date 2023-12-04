ee0e8d7547183ddb54fdfc25b7629f373713fca4
nicolas <nicolasecrespo@gmail.com>
2023-12-03 20:00:59 -0500

    updated assignments

--- cs/foo.py
+++ cs/foo.py
@@ -1,23 +1,11 @@
-def fib(n):
-    if n == 0:
-        return 0
-    elif n == 1:
-        return 1
-    else:
-        return fib(n - 1) + fib(n - 2)
+P = 5000
+r = 0.05
+t = 1 / 12
 
-
-n = int(input("Enter the number of terms: "))
-for i in range(n):
-    print(fib(i), end=" ")
-
-
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-
-
-n = int(input("Enter the number: "))
-print(f"Factorial of {n} is {factorial(n)}")
+while t <= 6 / 12:
+    interest = P * r * t
+    print(f"Month: {t*round(12)}")
+    print(f"Interest = ${interest}\nTotal = ${interest + P}")
+    print()
+    P = interest + P
+    t += 1 / 12
