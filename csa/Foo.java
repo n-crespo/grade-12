e169d22f86f0b04732bd90e75d3ae3c58130562e
Nicolas <nicolasecrespo@gmail.com>
2023-12-13 08:11:48 -0500

    updates/new assignments

--- csa/Foo.java
+++ csa/Foo.java
@@ -1,15 +1,5 @@
 public class Foo {
   public static void main(String[] args) {
-    String s = "It seems impossible until it's not.";
-    String target = "o";
-
-    int index = s.indexOf(target);
-
-    while (index >= 0) {
-      System.out.println("Location: " + index);
-      s = s.substring(0, index) + s.substring(index + 1);
-      index = s.indexOf(target);
-    }
-    System.out.println(s);
+    System.out.println(Math.PI);
   }
 }
