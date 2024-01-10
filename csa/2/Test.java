7190ccb3fe048283fd0b164a91b357baf4a50dc7
Nicolas <nicolasecrespo@gmail.com>
2024-01-10 12:41:05 -0500

    moving some things and upates to R assignments

---
+++ csa/2/Test.java
@@ -0,0 +1,7 @@
+public class Test {
+  public static void main(String[] args) {
+    String s1 = "banana";
+    int n = s1.length();
+    System.out.println(n);
+  }
+}
--- csa/Foo.java
+++ csa/Foo.java
@@ -1,7 +1,6 @@
 
 public class Foo {
   public static void main(String[] args) {
-    System.out.printf("%.3f", 3.22222222222222);
-    System.out.println("hello");
+
   }
 }
