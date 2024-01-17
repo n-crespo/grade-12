ca58d920fab95ae1a3e92505e6e4da5f670a3255
Nicolas <nicolasecrespo@gmail.com>
2024-01-16 19:17:13 -0500

    progress with hangman

--- csa/2/Hangman/Hangman.java
+++ csa/2/Hangman/Hangman.java
@@ -1,12 +1,15 @@
+import java.util.Scanner;
+
 public class Hangman {
 
   public static void main(String[] args) {
 
+    Scanner scan = new Scanner(System.in);
+    int randomNumber = (int) (Math.random() * 10) + 1;
+
     System.out.println("Welcome to Hangman! You have to guess the mystery word");
     System.out.println("in 5 guesses or less. ");
     System.out.println("Here's what you have so far: ");
-
-    int randomNumber = (int) (Math.random() * 10) + 1;
 
     String mysteryWord;
     if (randomNumber == 1) {
@@ -31,10 +34,46 @@ public class Hangman {
       mysteryWord = "universe";
     }
 
-    String hiddenWord = "*****";
+    int guesses = 5;
 
-    while (true) {
+    String guessString;
+    char guessChar;
+
+    String guessedLetters = "";
+
+    // make hidden word string
+    String hiddenWord = "";
+    for (int i = 0; i < mysteryWord.length(); i++) {
+      hiddenWord += "*";
+    }
+
+    while (guesses > 0) {
       System.out.println("Here's what you have so far: " + hiddenWord);
+      System.out.println("You have " + guesses + " guesses left. ");
+      System.out.println("Here are the letters you have guessed so far: ");
+
+      // print guessed letters
+      for (int i = 0; i < guessedLetters.length(); i++) {
+        System.out.print(guessedLetters.charAt(i) + " ");
+      }
+      System.out.print("What letter would you like to guess next? ");
+      guessString = scan.next();
+
+      // give me a char
+      guessChar = guessString.charAt(0);
+
+      for (int i = 0; i < mysteryWord.length(); i++) {
+        // if guess is correct
+        if (guessChar == mysteryWord.charAt(i)) {
+          System.out.println("CORRECT!!!");
+          for (int j = 0; j < mysteryWord.length(); j++) {
+            hiddenWord += "*";
+          }
+        }
+        System.out.println("I'm sorry...");
+        guesses -= 1;
+
+      }
 
     }
 
