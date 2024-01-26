556deb9aab2fb4b79e162accdd22a185eee2861d
Nicolas <nicolasecrespo@gmail.com>
2024-01-26 16:32:48 -0500

    small changes

--- csa/Foo.java
+++ csa/Foo.java
@@ -1,7 +1,11 @@
+import java.util.Scanner;
 
 public class Foo {
   public static void main(String[] args) {
     System.out.println("hello");
-
+    Scanner scan = new Scanner(System.in);
+    String inp = scan.nextLine();
+    System.out.println("You said" + inp);
+    scan.close();
   }
 }
