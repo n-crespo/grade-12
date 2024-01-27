d35cb38a4719dbd326e9a559602f01597b2a9717
Nicolas <nicolasecrespo@gmail.com>
2024-01-26 21:40:34 -0500

    formatting

--- csa/Foo.java
+++ csa/Foo.java
@@ -4,8 +4,9 @@ public class Foo {
   public static void main(String[] args) {
     System.out.println("hello");
     Scanner scan = new Scanner(System.in);
+    System.out.print("Say something: ");
     String inp = scan.nextLine();
-    System.out.println("You said" + inp);
+    System.out.println("You said " + inp);
     scan.close();
   }
 }
