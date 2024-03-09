e89dfc9189eb75873a9fc1dc0487b902c120c3e8
Nicolas <nicolasecrespo@gmail.com>
2024-03-09 09:01:19 -0500

    organization

--- cs/foo.py
+++ cs/foo.py
@@ -1,13 +1,18 @@
-import itertools
+def sieve_of_eratosthenes(limit):
+    # Create a boolean list "is_prime[0..n]" and initialize all elements as True
+    is_prime = [True] * (limit + 1)
+    is_prime[0] = is_prime[1] = False  # 0 and 1 are not prime numbers
 
-m = int(input())
-out = []
+    for num in range(2, int(limit**0.5) + 1):
+        if is_prime[num]:
+            # Mark all multiples of num as non-prime
+            for multiple in range(num * num, limit + 1, num):
+                is_prime[multiple] = False
 
-n = itertools.combinations_with_replacement([i for i in range(1, 7)], r=2)
+    # Return the list of prime numbers
+    return [num for num in range(2, limit + 1) if is_prime[num]]
 
-for i in n:
-    if sum(i) == m and sorted(i) not in out:
-        out.append(sorted(i))
 
-for i in out:
-    print(i)
+# Example: Finding primes up to 50
+primes_up_to_50 = sieve_of_eratosthenes(50)
+print(primes_up_to_50)
