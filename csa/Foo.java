36cfdaae5956e4b5cefcc78f8ea3a66caa6085c1
Nicolas <nicolasecrespo@gmail.com>
2024-03-28 19:39:04 -0500

    updates for some assignments

--- csa/Foo.java
+++ csa/Foo.java
@@ -1,14 +1,9 @@
 public class Foo {
-  public static String reverse(String str) {
-    String temp = "";
-    for (int i = str.length() - 1; i > 0; i--) {
-      temp += str.substring(i - 1, i);
-    }
-    return temp;
-  }
 
   public static void main(String[] args) {
-    System.out.println(reverse("apple"));
+    String x = "10";
+    String y = "11";
+    System.out.println(x + y);
 
   }
 }
