7cd14a2d86d26eec6c880ea3be83465ed8fd5820
Nicolas <nicolasecrespo@gmail.com>
2024-04-23 08:05:48 -0400

    updates

--- csa/1/firstPrograms/Program1.java
+++ csa/1/firstPrograms/Program1.java
@@ -1,10 +1,21 @@
+import java.util.ArrayList;
 import java.util.Scanner;
-public class Program1 {
-  public static void main(String[] args){
-  System.out.println("Enter your name: ");
-  Scanner scan = new Scanner(System.in);
-  String input = scan.nextLine();
 
-  System.out.println("Hi" + input + ", welcome to CSA!");
+public class Program1 {
+
+  public static void main(String[] args) {
+    int k;
+    int A[];
+    A = new int[7];
+    for (k = 0; k < A.length; k++) {
+      A[k] = A.length - k;
+    }
+    for (k = 0; k < A.length - 1; k++) {
+      A[k + 1] = A[k];
+    }
+
+    for (int i : A) {
+      System.out.println(i);
+    }
   }
 }
