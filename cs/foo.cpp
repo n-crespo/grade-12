3835b96c855c23fd42c3eebea8f8c1b5c2f47e09
Nicolas <nicolasecrespo@gmail.com>
2024-04-26 14:21:07 -0400

    update submodules

---
+++ cs/foo.cpp
@@ -0,0 +1,8 @@
+#include <cmath>
+#include <cstdlib> // For rand() and srand()
+#include <ctime>   // For time()
+#include <iostream>
+
+using namespace std;
+
+int main() { cout << exp(1 / 2) << endl; }
---
+++ csa/Foo.java
@@ -0,0 +1,10 @@
+/**
+ * Foo
+ */
+public class Foo {
+
+  public static void main(String[] args) {
+    String s = "I like pizza";
+    System.out.println(s.charAt(1));
+  }
+}
