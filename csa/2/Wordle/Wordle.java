a1053ba61083acf143fee03cf59f1718481ea402
Nicolas <nicolasecrespo@gmail.com>
2024-05-13 09:03:28 -0400

    finish wordle

--- csa/2/Wordle/Wordle.java
+++ csa/2/Wordle/Wordle.java
@@ -1,4 +1,3 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 /**
@@ -14,7 +13,16 @@ public class Wordle {
     return false;
   }
 
-  public static boolean charInWord(String word, String guess) {
+  // if letter has been used, replace letter with # in used words list
+  public static void addToUsedWords(String word, char usedLetters[]) {
+    for (int j = 0; j < usedLetters.length; j++) {
+      if (word.contains(usedLetters[j] + "")) {
+        usedLetters[j] = '#';
+      }
+    }
+  }
+
+  public static boolean charInGuess(String word, String guess) {
     for (int i = 0; i < word.length(); i++) {
       if (word.contains(guess.charAt(i) + "")) {
         return true;
@@ -23,35 +31,65 @@ public class Wordle {
     return false;
   }
 
+  public static void printScreen(char guesses[][], char usedLetters[]) {
+    for (int i = 0; i < guesses.length; i += 2) {
+      for (int j = 0; j < guesses[i].length; j++) {
+        System.out.println("| " + guesses[i][j] + " | ");
+      }
+    }
+    for (int i = 0; i < usedLetters.length; i++) {
+      System.out.print(usedLetters[i] + " ");
+    }
+  }
+
   public static void main(String[] args) {
+    // array of the alphabet
+    char usedLetters[] = new char[26];
+    for (int i = 0; i < usedLetters.length; i++) {
+      usedLetters[i] = (char) (65 + i);
+    }
+    int lettersCorrect = 0;
+    int numAttempts = 0;
+    char guesses[][] = new char[12][5];
+    int currentRow = 0;
     Scanner scanner = new Scanner(System.in);
+
+    // header + word to guess
     System.out.println("***Welcome to Wordle!***");
     System.out.println("Have someone enter in a five letter word for you to guess, all caps:");
     String word = scanner.nextLine();
     System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
-    // clear the screen
-    // System.out.print("\033[H\033[2J");
-    // System.out.flush();
+    while (lettersCorrect < 5 && numAttempts < 6) {
+      System.out.println("Enter a 5 letter word in all caps: ");
+      String guess = scanner.nextLine();
+      lettersCorrect = 0;
+      numAttempts++;
 
-    System.out.println("Enter a 5 letter word in all caps: ");
-    String guess = scanner.nextLine();
-    ArrayList<Character> guesses = new ArrayList<Character>();
-    for (int i = 0; i < guess.length(); i++) {
-      guesses.add(guess.charAt(i));
-      if (guess.charAt(i) == word.charAt(i)) {
-        System.out.println("*");
-      }
-
-      boolean done = false;
-      while (!done) {
-        for (int j = 0; j < word.length(); j++) {
-          if (guess.charAt(i) == word.charAt(j) && !done) {
-            System.out.println("@");
-            done = true;
-          }
+      for (int i = 0; i < guess.length(); i++) {
+        guesses[currentRow][i] = guess.charAt(i);
+        if (charInCorrectPosition(word, guess)) {
+          guesses[currentRow + 1][i] = '*';
+          lettersCorrect++;
+        } else if (charInGuess(word, guess)) {
+          guesses[currentRow + 1][i] = '@';
+        } else {
+          guesses[currentRow + 1][i] = 'x';
         }
       }
+      currentRow += 2;
+
+      addToUsedWords(guess, usedLetters);
+      printScreen(guesses, usedLetters);
+    }
+
+    // end screen
+    if (lettersCorrect == 5) {
+      System.out.println("You won!");
+      System.out.println("You used " + numAttempts + " guesses.");
+    } else if (numAttempts == 6) {
+      System.out.println("You ran out of guesses!");
+
     }
     scanner.close();
   }
--- csa/Foo.java
+++ csa/Foo.java
@@ -1,17 +1,16 @@
-package csa;
-
-import java.util.ArrayList;
-
 /**
  * Foo
  */
 public class Foo {
   public static void main(String[] args) {
-    ArrayList<String> list = new ArrayList<String>();
-    int i = 5;
-    double d = (double) i;
-    int r = (int) d;
-    System.out.println(d);
-    System.out.println(r);
+
+    char usedLetters[] = new char[26];
+    for (int i = 0; i < usedLetters.length; i++) {
+      usedLetters[i] = (char) (65 + i);
+    }
+
+    for (int i = 0; i < usedLetters.length; i++) {
+      System.out.println(usedLetters[i]);
+    }
   }
 }
