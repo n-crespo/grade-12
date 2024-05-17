45aac8bc3f92769483aa3b413dc536be9d1fa549
Nicolas <nicolasecrespo@gmail.com>
2024-05-17 08:13:44 -0400

    progress in wordle

--- csa/2/Wordle/Wordle.java
+++ csa/2/Wordle/Wordle.java
@@ -4,42 +4,45 @@ import java.util.Scanner;
  * Wordle
  */
 public class Wordle {
-  public static boolean charInCorrectPosition(String word, String guess) {
-    for (int i = 0; i < word.length(); i++) {
-      if (guess.charAt(i) == word.charAt(i)) {
-        return true;
-      }
+  public static boolean charInCorrectPosition(String word, char guessedLetter, int pos) {
+    if (word.charAt(pos) == guessedLetter) {
+      return true;
+    }
+    return false;
+  }
+
+  public static boolean charInGuess(String word, char gussedLetter) {
+    if (word.contains(gussedLetter + "")) {
+      return true;
     }
     return false;
   }
 
   // if letter has been used, replace letter with # in used words list
-  public static void addToUsedWords(String word, char usedLetters[]) {
+  public static void addToUsedWords(String guess, char usedLetters[]) {
     for (int j = 0; j < usedLetters.length; j++) {
-      if (word.contains(usedLetters[j] + "")) {
+      if (guess.contains(usedLetters[j] + "")) {
         usedLetters[j] = '#';
       }
     }
   }
 
-  public static boolean charInGuess(String word, String guess) {
-    for (int i = 0; i < word.length(); i++) {
-      if (word.contains(guess.charAt(i) + "")) {
-        return true;
+  public static void printScreen(char allGuesses[][], char usedLetters[], int currentRow) {
+    for (int i = 0; i < allGuesses.length; i++) {
+      System.out.print("|");
+      for (int j = 0; j < allGuesses[i].length; j++) {
+        if (i < currentRow) {
+          System.out.print(allGuesses[i][j]);
+        } else {
+          System.out.print(" ");
+        }
       }
-    }
-    return false;
-  }
-
-  public static void printScreen(char guesses[][], char usedLetters[]) {
-    for (int i = 0; i < guesses.length; i += 2) {
-      for (int j = 0; j < guesses[i].length; j++) {
-        System.out.println("| " + guesses[i][j] + " | ");
-      }
+      System.out.println("|");
     }
     for (int i = 0; i < usedLetters.length; i++) {
       System.out.print(usedLetters[i] + " ");
     }
+    System.out.println("\n");
   }
 
   public static void main(String[] args) {
@@ -50,7 +53,7 @@ public class Wordle {
     }
     int lettersCorrect = 0;
     int numAttempts = 0;
-    char guesses[][] = new char[12][5];
+    char allGuesses[][] = new char[12][5];
     int currentRow = 0;
     Scanner scanner = new Scanner(System.in);
 
@@ -61,26 +64,26 @@ public class Wordle {
     System.out.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
 
     while (lettersCorrect < 5 && numAttempts < 6) {
-      System.out.println("Enter a 5 letter word in all caps: ");
-      String guess = scanner.nextLine();
+      System.out.print("Enter a 5 letter word in all caps: ");
+      String guess = scanner.next();
       lettersCorrect = 0;
       numAttempts++;
 
       for (int i = 0; i < guess.length(); i++) {
-        guesses[currentRow][i] = guess.charAt(i);
-        if (charInCorrectPosition(word, guess)) {
-          guesses[currentRow + 1][i] = '*';
+        allGuesses[currentRow][i] = guess.charAt(i);
+        if (charInCorrectPosition(word, guess.charAt(i), i)) {
+          allGuesses[currentRow + 1][i] = '*';
           lettersCorrect++;
-        } else if (charInGuess(word, guess)) {
-          guesses[currentRow + 1][i] = '@';
+        } else if (charInGuess(word, guess.charAt(i))) {
+          allGuesses[currentRow + 1][i] = '@';
         } else {
-          guesses[currentRow + 1][i] = 'x';
+          allGuesses[currentRow + 1][i] = 'x';
         }
       }
       currentRow += 2;
 
       addToUsedWords(guess, usedLetters);
-      printScreen(guesses, usedLetters);
+      printScreen(allGuesses, usedLetters, currentRow);
     }
 
     // end screen
--- csa/Foo.java
+++ csa/Foo.java
@@ -1,16 +1,21 @@
+package csa;
+
 /**
  * Foo
  */
 public class Foo {
+
+  public static void mod(int[] a, int value) {
+    for (int i = 0; i < a.length; i++) {
+      a[i] = i;
+    }
+    value = a[a.length - 1];
+  }
+
   public static void main(String[] args) {
+    int[] itemArray = { 9, 8, 7, 6 };
+    int val = 5;
+    mod(itemArray, val);
 
-    char usedLetters[] = new char[26];
-    for (int i = 0; i < usedLetters.length; i++) {
-      usedLetters[i] = (char) (65 + i);
-    }
-
-    for (int i = 0; i < usedLetters.length; i++) {
-      System.out.println(usedLetters[i]);
-    }
   }
 }
